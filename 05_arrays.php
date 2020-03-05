60d05221eef9cfe4291ebc5138bbe2e4752870fe
Lawrence <locaulker@gmail.com>
2020-03-05 06:56:05 -0500

    reviewed index arrays

---
+++ 05_arrays.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Introduction to PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+    
+    <div class="container">
+      <h1>Up & Running with PHP</h1>
+      <div class="code-content">
+      <?php
+        $fruits = ['apple', 'orange', 'Melon', 'Banana'];
+
+        array_push($fruits, 'pear');
+          echo $fruits[4];
+
+        unset($fruits[1]);
+
+        echo '<pre>';
+          var_dump($fruits);
+        echo '</pre>';
+      ?>
+      </div>
+    </div>
+    
+  </body>
+</html>
